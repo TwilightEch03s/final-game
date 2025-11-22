fe6bcf470a66a2ea9faf5f79afe281c2a3651b82
Stan-21 <77824280+Stan-21@users.noreply.github.com>
2025-11-22 02:28:55 +0000

    formating changes

--- .vite/deps/vite.config.ts
+++ .vite/deps/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/<repo>/', // replace <repo> with your GitHub repo name
-})
+  base: "/<repo>/", // replace <repo> with your GitHub repo name
+});
